5e3a0b3fdc688a31c995cc818ebab570e02192b3
unknown <rislammb@gmail.com>
2022-12-08 15:03:13 +0600

    change for api config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-// export const axiosInstance = axios.create({
-//   baseURL: process.env.REACT_APP_API_URL,
-// });
-
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_API_URL,
 });
+
+// export const axiosInstance = axios.create({
+//   baseURL: 'http://localhost:5000/api',
+// });
