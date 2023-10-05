06db375454db85a69fd5a4395d5500955bc9c969
Rayhanul Islam <rislammb@gmail.com>
2023-10-05 09:08:45 +0600

    Add store rehydrate for getting data from store

--- src/hooks/useApp.js
+++ src/hooks/useApp.js
@@ -1,5 +1,5 @@
 import { createTheme } from '@mui/material/styles';
-import { useStoreState } from 'easy-peasy';
+import { useStoreState, useStoreRehydrated } from 'easy-peasy';
 import { useEffect, useMemo, useState } from 'react';
 import { setAuthToken } from '../api/config';
 
@@ -7,8 +7,9 @@ const useApp = () => {
   const [mode, setMode] = useState(
     localStorage.getItem('BDPA_COLOR_MODE') ?? 'light'
   );
+  const isRehydrated = useStoreRehydrated();
   const { token } = useStoreState((state) => state.auth);
-  if (token) setAuthToken(token);
+  if (isRehydrated && token) setAuthToken(token);
 
   const colorMode = useMemo(
     () => ({
@@ -46,6 +47,7 @@ const useApp = () => {
   }, [mode]);
 
   return {
+    isRehydrated,
     mode,
     colorMode,
     theme,
