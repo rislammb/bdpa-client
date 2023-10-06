b1b4cd4ce4edde7a3e3a57ca2d151d7247800cb1
Rayhanul Islam <rislammb@gmail.com>
2023-10-06 17:06:48 +0600

    Add bengali information on jobDepertment.js

--- src/constants/jobDepertment.js
+++ src/constants/jobDepertment.js
@@ -2,21 +2,24 @@ export const jobDepertmentOptions = [
   {
     id: '1',
     name: 'Directorate General of Family Planning',
+    name_bn: 'পরিবার পরিকল্পনা অধিদপ্তর',
   },
   {
     id: '2',
     name: 'Directorate General of Health Services',
+    name_bn: 'স্বাস্থ্য অধিদপ্তর',
   },
 
-  { id: '3', name: 'Bangladesh Railway' },
+  { id: '3', name: 'Bangladesh Railway', name_bn: 'বাংলাদেশ রেলওয়ে', },
   {
     id: '4',
     name: 'Border Guard of Bangladesh',
+    name_bn: 'বর্ডার গার্ড বাংলাদেশ',
   },
-  { id: '5', name: 'Bangladesh Police' },
-  { id: '6', name: 'Bangladesh Bank' },
-  { id: '17', name: 'Pharmacy/Medicine Store' },
-  { id: '18', name: 'Non-Governmental Organization' },
+  { id: '5', name: 'Bangladesh Police', name_bn: 'বাংলাদেশ পুলিশ', },
+  { id: '6', name: 'Bangladesh Bank', name_bn: 'বাংলাদেশ ব্যাংক', },
+  { id: '17', name: 'Pharmacy/Medicine Store', name_bn: 'ফার্মেসী/ঔষধের দোকান', },
+  { id: '18', name: 'Non-Governmental Organization', name_bn: 'বেসরকারী সংস্থা', },
 ];
 
 export const jobDepertmentField = {
@@ -25,10 +28,12 @@ export const jobDepertmentField = {
     {
       id: '0',
       name: '--- Select ---',
+      name_bn: '--- নির্বাচন ---',
     },
     ...jobDepertmentOptions,
   ],
   name: 'jobDepertment',
   label: 'Job Depertment',
+  label_bn: 'চাকুরীর বিভাগ',
   value: '0',
 };
