afa3e8c0c05e7670856cc8f45ec41abf7409437d
unknown <rislammb@gmail.com>
2023-10-23 15:13:13 +0600

    add language to persist for save on local storage

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,10 @@ import pharmacistModel from './pharmacistModel';
 import uiModel from './uiModel';
 
 const store = createStore({
-  ui: persist(uiModel, { allow: ['mode'], storage: 'localStorage' }),
+  ui: persist(uiModel, {
+    allow: ['mode', 'language'],
+    storage: 'localStorage',
+  }),
   auth: persist(authModel, { allow: ['token'], storage: 'localStorage' }),
   pharmacist: persist(pharmacistModel, { allow: ['list', 'details'] }),
   committee: persist(committeeModel, { allow: ['list', 'details'] }),
