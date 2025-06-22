5894ee0db0d5357f6c76dccbdcc96f21ed4ac9ac
Rayhanul Islam <rislammb@gmail.com>
2025-06-22 06:55:57 +0600

    updated date picker format

--- src/components/DatePickerComp.jsx
+++ src/components/DatePickerComp.jsx
@@ -1,8 +1,8 @@
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import dayjs from 'dayjs';
-import PropTypes from 'prop-types';
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import dayjs from "dayjs";
+import PropTypes from "prop-types";
 
 const DatePickerComp = ({
   name,
@@ -12,8 +12,8 @@ const DatePickerComp = ({
   disableFuture,
   referenceDate,
   maxDate,
-  openTo = 'day',
-  views = ['day', 'month', 'year'],
+  openTo = "day",
+  views = ["day", "month", "year"],
   error,
   helperText,
 }) => (
@@ -27,10 +27,11 @@ const DatePickerComp = ({
       disableFuture={disableFuture}
       referenceDate={referenceDate && dayjs(referenceDate)}
       maxDate={maxDate && dayjs(maxDate)}
+      format="DD/MMM/YYYY"
       slotProps={{
         textField: {
-          variant: 'standard',
-          InputLabelProps: { color: 'info' },
+          variant: "standard",
+          InputLabelProps: { color: "info" },
           error,
           helperText,
         },
