d33a89b23eba134a30997af8ce96a58aa61a3b6f
Rayhanul Islam <rislammb@gmail.com>
2025-09-29 07:43:54 +0600

    updated gender fields

--- src/constants/gender.js
+++ src/constants/gender.js
@@ -1,22 +1,21 @@
 export const genderOptions = [
-  { id: '1', name: 'Male', bn_name: 'পুরুষ' },
-  { id: '2', name: 'Female', bn_name: 'মহিলা' },
-  { id: '3', name: 'Other', bn_name: 'অন্যান্য' },
+  { id: "1", name: "Male", bn_name: "পুরুষ" },
+  { id: "2", name: "Female", bn_name: "মহিলা" },
 ];
 
 export const genderOptionsWithEmpty = [
-  { id: '', name: '--- Select ---', bn_name: '--- নির্বাচন ---' },
+  { id: "", name: "--- Select ---", bn_name: "--- নির্বাচন ---" },
   ...genderOptions,
 ];
 
 export const genderField = {
-  type: 'select',
-  name: 'gender',
-  label: 'Gender',
-  bn_label: 'লিঙ্গ',
+  type: "select",
+  name: "gender",
+  label: "Gender",
+  bn_label: "লিঙ্গ",
   options: [
-    { id: '', name: '--- Select ---', bn_name: '--- নির্বাচন ---' },
+    { id: "", name: "--- Select ---", bn_name: "--- নির্বাচন ---" },
     ...genderOptions,
   ],
-  value: '',
+  value: "",
 };
